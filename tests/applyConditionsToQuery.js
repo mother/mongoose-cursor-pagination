d5c1d9323990e516c1a191496ff9c75bfb5f8e62
Ajay Sabhaney <ajay@mothercreative.com>
2019-02-14 23:11:41 -0500

    More fixes, tests and cleanup

---
+++ tests/applyConditionsToQuery.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose')
+const { applyConditionsToQuery } = require('../lib/utils')
+
+const Comment = mongoose.model('comment')
+
+const singleKeyCursorCondition = [{
+   _id: { $lt: '5b06b90b42a0b29ba10f20c2' }
+}]
+
+const multiKeyCursorConditions = [
+   { 'author.lastName': { $gt: 'Doe' } },
+   {
+      'author.lastName': 'Doe',
+      _id: { $lt: '5b06b90b42a0b29ba10f20c2' }
+   }
+]
+
+describe('utils#applyConditionsToQuery', () => {
+   it('Works with single cursor keys', () => {
+      let query = Comment.find({ body: 'Test' })
+      query = applyConditionsToQuery(singleKeyCursorCondition, query)
+      expect(query.getQuery()).toMatchSnapshot()
+   })
+
+   it('Works with multiple cursor keys with no other $or query conditions', () => {
+      let query = Comment.find({ body: 'Test' })
+      query = applyConditionsToQuery(multiKeyCursorConditions, query)
+      expect(query.getQuery()).toMatchSnapshot()
+   })
+
+   it('Works with multiple cursor keys when there is an $or condition in the query', () => {
+      let query = Comment.find({
+         $or: [
+            { body: 'Test' },
+            { 'author.firstName': 'Oreo' }
+         ]
+      })
+
+      query = applyConditionsToQuery(multiKeyCursorConditions, query)
+      expect(query.getQuery()).toMatchSnapshot()
+   })
+
+   it('Works with multiple cursor keys when there is an $or and $and condition in the query', () => {
+      let query = Comment.find({
+         $and: [
+            { body: 'body is in an and' },
+            { 'author.firstName': 'Andy' }
+         ],
+         $or: [
+            { body: 'body is in an or' },
+            { 'author.firstName': 'Oreo' }
+         ]
+      })
+
+      query = applyConditionsToQuery(multiKeyCursorConditions, query)
+      expect(query.getQuery()).toMatchSnapshot()
+   })
+})
